dd34b71c26ddcc7c56b1cd69bae6e8db5d976111
David Ward <david.a.ward@duke.edu>
2020-06-16 09:14:52 -0400

    Add calculator code

---
+++ code.py
@@ -0,0 +1,24 @@
+def add_numbers(a, b, c, d):
+    answer = a + b + c + d
+    return answer
+
+
+def subtract_numbers (a, b, c):
+    answer = a - b - c
+    return answer
+
+
+def main_code():
+    a = 10
+    b = 1
+    c = 5
+    d = 7
+    x = add_numbers(a, b, c, d)
+    y = subtract_numbers(a, b, c, d)
+    print("Add = {}".format(x))
+    print("Subtract = {}".format(y)
+    return
+    
+    
+if __name__ == "__main__":
+    main_code()
